a219a30816f4d26f06c49a253944b05dc3fa133d
Артем Черенков <art.cherenkov@gmail.com>
2021-08-26 13:07:59 +0700

    feat: подключить шрифты, normalize и CSS-переменные

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./components/App";
 
+import "./vendor/variables.css";
+import "./vendor/normalize.css";
+import "./vendor/fonts.css";
+import "./index.css";
+
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
