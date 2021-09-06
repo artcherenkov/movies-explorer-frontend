54c3291028d46ee115e7fca212c1aaa6340e484a
Артем Черенков <art.cherenkov@gmail.com>
2021-09-06 13:30:43 +0700

    feat: адаптировать "hero" к 768

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,7 @@ const Hero = ({ className }) => (
     <div className="hero__container">
       <h2 className="hero__title">
         Учебный проект студента факультета
-        <br />
+        <br className="hero__title-divider" />
         Веб-разработки.
       </h2>
       <p className="hero__text">
