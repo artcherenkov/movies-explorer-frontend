7dcd3b2baa140d7b49f0cb359edd68f438ce74cc
Артем Черенков <art.cherenkov@gmail.com>
2021-09-11 16:18:35 +0700

    feat: адаптировать навигацию к мобильным

--- src/components/Header/components/AuthNav.js
+++ src/components/Header/components/AuthNav.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 const AuthNav = () => (
-  <div className="header__nav header__nav_auth">
+  <div className="header__nav header__nav_type_auth">
     <Link className="link header__link header__link_type_auth" to="/signup">
       Регистрация
     </Link>
